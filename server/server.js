fcb8d1ada43c0b8830247baced8a28c72095b687
Paulius Malinauskas <paulius.malinauskas@rantandrave.com>
2018-10-10 20:06:57 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express')
+const publicPath = path.join(__dirname,'/../public');
+
+var app = express();
+
+var port = process.env.PORT||3000;
+
+app.use(express.static(publicPath))
+
+
+
+app.listen(port, () => {
+    console.log(`Started server at port: ${port}`);
+})
+
+module.exports={app}
+
