c30664f40c927af3882359da0117ae0b3bd60d75
Paulius Malinauskas <paulius.malinauskas@rantandrave.com>
2018-10-11 20:23:51 +0100

    emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect',function () {
     console.log('Connected to server')
 
-    socket.emit('createMessage', {
-        to: 'Phoebe@gmail.com',
-        text: 'what up mate?'
-    })
 })
 
 socket.on('disconnect',function () {
